ff3a934764d2cc04906b0cbaf43b36cfc609f9d4
whlionel704 <whlionel704@gmail.com>
2025-10-23 12:10:58 +0800

    learn

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+
+module.exports = {
+  //Ensures that the helpers and integrations from the Viem plugin are available
+  plugins: [hardhatToolboxViemPlugin],
+  networks: {
+    besu: {
+      type: "http",
+      url: process.env, // RPC of your Besu node
+      accounts: [
+        process.env.ALLOC_1_PRIVATE_KEY!, 
+        process.env.ALLOC_2_PRIVATE_KEY!, 
+        process.env.ALLOC_3_PRIVATE_KEY!
+      ] // From genesis.json
+    }
+  },
+  solidity: { 
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    }
+  }
+};
+
+
